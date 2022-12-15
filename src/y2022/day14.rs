611e86a2c8c353008ab7bbf57d6964795ad1f991
boris <boris.lok@cienet.com>
2022-12-15 08:37:41 +0800

    [Refactoring] Change day14 coding style.

--- src/y2022/day14.rs
+++ src/y2022/day14.rs
@@ -1,59 +1,54 @@
-use std::cmp::{max, min};
 use itertools::Itertools;
+use std::cmp::{max, min};
+
+fn parse_input(input: &str) -> (Vec<Vec<i8>>, usize) {
+    let mut grid = vec![vec![0_i8; 1000]; 1000];
+    let mut abs_y = 0;
+    input
+        .lines()
+        .into_iter()
+        .map(|e| e.split(" -> ").collect::<Vec<_>>())
+        .for_each(|p| {
+            p.windows(2).for_each(|ee| {
+                let first = ee.first().unwrap();
+                let last = ee.last().unwrap();
+
+                let first = string_to_coordinate(first);
+                let last = string_to_coordinate(last);
+
+                abs_y = max(max(abs_y, first.1), last.1);
+
+                let min_x = min(first.0, last.0);
+                let max_x = max(first.0, last.0);
+                let min_y = min(first.1, last.1);
+                let max_y = max(first.1, last.1);
+
+                for y in min_y..=max_y {
+                    for x in min_x..=max_x {
+                        grid[y][x] = -1;
+                    }
+                }
+            })
+        });
+
+    (grid, abs_y)
+}
+
+fn string_to_coordinate(input: &str) -> (usize, usize) {
+    input
+        .split(",")
+        .map(|e| e.parse::<usize>().unwrap())
+        .collect_tuple::<(usize, usize)>()
+        .unwrap()
+}
 
 pub fn puzzle_a(input: &str) -> usize {
-    let mut grid = vec![vec![0_i8; 1000]; 1000];
-
-    let lines = input.lines()
-        .collect::<Vec<_>>();
-
-    let part = lines
-        .iter()
-        .map(|&e| {
-            e.split(" -> ")
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-
-    let mut abs_y = 0;
-
-    for p in part {
-        p.windows(2)
-            .for_each(|e| {
-                let first = e.first().unwrap();
-                let last = e.last().unwrap();
-
-                let first = first.split(",")
-                    .map(|e| e.parse::<usize>().unwrap()).collect_tuple::<(usize, usize)>()
-                    .unwrap();
-
-                let last = last.split(",")
-                    .map(|e| e.parse::<usize>().unwrap()).collect_tuple::<(usize, usize)>()
-                    .unwrap();
-
-                let begin = min(first.0, last.0);
-                let end = max(first.0, last.0);
-
-                abs_y = max(first.1, abs_y);
-                abs_y = max(last.1, abs_y);
-
-                for dx in begin..=end {
-                    grid[first.1][dx] = -1;
-                }
-
-                let begin = min(first.1, last.1);
-                let end = max(first.1, last.1);
-
-                for dy in begin..=end {
-                    grid[dy][first.0] = -1;
-                }
-            });
-    }
+    let (mut grid, abs_y) = parse_input(input);
 
     let mut c = 0;
 
     loop {
-        if let Some(p) = move_sand((abs_y + 2) as isize, (500, 0), &grid) {
+        if let Some(p) = move_sand_for_puzzle_a((abs_y + 2) as isize, (500, 0), &grid) {
             grid[p.1 as usize][p.0 as usize] = -2;
             c += 1;
         } else {
@@ -64,58 +59,12 @@ pub fn puzzle_a(input: &str) -> usize {
 }
 
 pub fn puzzle_b(input: &str) -> usize {
-    let mut grid = vec![vec![0_i8; 1000]; 1000];
-
-    let lines = input.lines()
-        .collect::<Vec<_>>();
-
-    let part = lines
-        .iter()
-        .map(|&e| {
-            e.split(" -> ")
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-
-    let mut abs_y = 0;
-
-    for p in part {
-        p.windows(2)
-            .for_each(|e| {
-                let first = e.first().unwrap();
-                let last = e.last().unwrap();
-
-                let first = first.split(",")
-                    .map(|e| e.parse::<usize>().unwrap()).collect_tuple::<(usize, usize)>()
-                    .unwrap();
-
-                let last = last.split(",")
-                    .map(|e| e.parse::<usize>().unwrap()).collect_tuple::<(usize, usize)>()
-                    .unwrap();
-
-                let begin = min(first.0, last.0);
-                let end = max(first.0, last.0);
-
-                abs_y = max(first.1, abs_y);
-                abs_y = max(last.1, abs_y);
-
-                for dx in begin..=end {
-                    grid[first.1][dx] = -1;
-                }
-
-                let begin = min(first.1, last.1);
-                let end = max(first.1, last.1);
-
-                for dy in begin..=end {
-                    grid[dy][first.0] = -1;
-                }
-            });
-    }
+    let (mut grid, abs_y) = parse_input(input);
 
     let mut c = 0;
 
     loop {
-        if let Some(p) = move_sand_2((abs_y + 2) as isize, (500, 0), &grid) {
+        if let Some(p) = move_sand_for_puzzle_b((abs_y + 2) as isize, (500, 0), &grid) {
             grid[p.1 as usize][p.0 as usize] = -2;
             c += 1;
             if p.0 == 500 && p.1 == 0 {
@@ -126,8 +75,11 @@ pub fn puzzle_b(input: &str) -> usize {
     c
 }
 
-
-fn move_sand(abs_y: isize, current_position: (isize, isize), grid: &[Vec<i8>]) -> Option<(isize, isize)> {
+fn move_sand_for_puzzle_a(
+    abs_y: isize,
+    current_position: (isize, isize),
+    grid: &[Vec<i8>],
+) -> Option<(isize, isize)> {
     if current_position.1 == abs_y {
         return None;
     }
@@ -143,14 +95,18 @@ fn move_sand(abs_y: isize, current_position: (isize, isize), grid: &[Vec<i8>]) -
         }
 
         if grid[dy as usize][dx as usize] == 0 {
-            return move_sand(abs_y, (dx, dy), grid);
+            return move_sand_for_puzzle_a(abs_y, (dx, dy), grid);
         }
     }
 
     return Some(current_position);
 }
 
-fn move_sand_2(abs_y: isize, current_position: (isize, isize), grid: &[Vec<i8>]) -> Option<(isize, isize)> {
+fn move_sand_for_puzzle_b(
+    abs_y: isize,
+    current_position: (isize, isize),
+    grid: &[Vec<i8>],
+) -> Option<(isize, isize)> {
     if current_position.1 == abs_y {
         return None;
     }
@@ -166,7 +122,7 @@ fn move_sand_2(abs_y: isize, current_position: (isize, isize), grid: &[Vec<i8>])
         }
 
         if grid[dy as usize][dx as usize] == 0 {
-            if let Some(position) = move_sand_2(abs_y, (dx, dy), grid) {
+            if let Some(position) = move_sand_for_puzzle_b(abs_y, (dx, dy), grid) {
                 return Some(position);
             } else {
                 continue;
